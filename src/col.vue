d312d1f1417ba64c9bade86f83734289db29bb7b
Clay_wang <Clay_wang@vip.163.com>
2020-06-23 17:43:51 +0800

    Row和Col的gutter(自由间隙)基本完成

---
+++ src/col.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+  :style="{paddingLeft: gutter/2 + 'px',paddingRight: gutter/2 + 'px'}">
+    <slot></slot>
+  </div>
+
+</template>
+
+<script>
+  export default {
+   name: "wkcCol",
+    props:{
+     span:{
+       type:[Number,String]
+     },
+     offset:{
+       type:[Number,String]
+     }
+    },
+    data(){
+     return{
+       gutter: 0
+     }
+    }
+  }
+
+</script>
+
+<style scoped lang="scss">
+  .col{
+    height: 100px;
+    width: 50%;
+    border: 1px solid red;
+
+    $class-prefix: col-;
+      // loops through 24 times
+      @for $n from 1 through 24 {
+
+        // for each $col-#{n}
+        &.#{$class-prefix}#{$n} {
+
+          width: ($n/24)*100%
+        }
+      }
+    $class-prefix: offset-;
+      // loops through 24 times
+      @for $n from 1 through 24 {
+
+        // for each $col-#{n}
+        &.#{$class-prefix}#{$n} {
+
+          margin-left: ($n/24)*100%
+        }
+      }
+  }
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row" :style="{marginRight: -gutter/2+'px',marginLeft: -gutter/2+'px'}">
+    <slot></slot>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "wkcRow",
+    props:{
+      gutter:{
+        type:[Number,String]
+      }
+    },
+    mounted() {
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .row{
+    display: flex;
+  }
+
+</style>
