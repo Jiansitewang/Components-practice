28cc8c4847116cb70497e12e5c42df48b9c36715
Clay_wang <Clay_wang@vip.163.com>
2020-07-21 10:04:08 +0800

    引入vuepress,初步配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'wkcUI',
+  description: '学习vue',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        children: ['/components/button']
+      }
+    ]
+  }
+}
